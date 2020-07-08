7749641bf559dad2b290e2ab0cd631a8d312424f
kaier <kaier@steam001.com>
2020-07-08 19:59:07 +0800

    lolpachong

---
+++ docs/.vuepress/components/lolskin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span @click="download">
+    {{ downUrl ? '点击下载' : 'loading.....' }}
+    </span>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        downUrl:''
+      }
+    },
+    mounted () {
+      this.getUrl()
+    },
+    methods: {
+      download() {
+        window.open(this.downUrl)
+      },
+      getUrl() {
+        axios.get('http://127.0.0.1:7002/api/v2/spider/lolskin').then((res)=>{
+          this.downUrl = res.data.data || ''
+        })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+span{
+  cursor: pointer;
+}
+</style>
---
+++ docs/views/游戏/lol.md
@@ -0,0 +1,14 @@
+---
+title: LOL皮肤插件（跟官网保持同步更新）
+date: 2020-07-08
+tags:
+ - 游戏
+categories:
+ -  游戏
+# keys:
+#  - '123456'
+---
+
+
+<!--more -->
+<lolskin />
